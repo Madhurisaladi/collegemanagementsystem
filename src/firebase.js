fe221c3f242546dabf2082c163e14eab6a70e003
Yarra Eswaraiah <eswaraiahyarra2004@gmail.com>
2025-02-24 14:31:29 +0530

    document uploads

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMXrMouOMfUK1GTE9-7I2FCyeSpr1zop4",
   authDomain: "college-management-proje-70ee7.firebaseapp.com",
   projectId: "college-management-proje-70ee7",
-  storageBucket: "college-management-proje-70ee7.appspot.com", // Corrected storage bucket URL
+  storageBucket: "gs://college-management-proje-70ee7.firebasestorage.app",
   messagingSenderId: "677615855448",
   appId: "1:677615855448:web:e5ebc76ed272a27417a72d",
   measurementId: "G-XZKHQQ83WJ"
